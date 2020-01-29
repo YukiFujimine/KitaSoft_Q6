989cda1de4fc664ef0e0b3b1dd6d4df4cc95d39c
YukiFujimine <study.yuki0115@gmail.com>
2020-01-29 14:45:28 +0900

    北ソフトQ6_3

--- Q6_3.java
+++ Q6_3.java
@@ -5,15 +5,17 @@ public class Q6_3{
 		int inputX=new Scanner(System.in).nextInt();
 		System.out.print("整数yを入力してください>");
 		int inputY=new Scanner(System.in).nextInt();
-		max(inputX,inputY);
-		System.out.printf("大きいのは%dです",max(inputX,inputY));
+		System.out.print("整数zを入力してください>");
+		int inputZ=new Scanner(System.in).nextInt();
+		System.out.printf("大きいのは"+max((max(inputX,inputY)),inputZ)+"です。");
 	}
-	static int max(int x,int y){
-		if(x>y){
-			z=x;
+	static int max(int i,int j){
+		int ans=0;
+		if(i>j){
+			ans=i;
 		}else{
-			z=y;
+			ans=j;
 		}
-		return z;
+		return ans;
 	}
 }
